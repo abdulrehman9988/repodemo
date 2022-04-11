fd4d0d7ea49f59ff03c727408f60a6ebfd03161f
abdulrehman9988 <a.rehmanrg@gmail.com>
2022-04-11 10:32:09 +0530

    kk

--- src/TailwindCss/Navbar.js
+++ src/TailwindCss/Navbar.js
@@ -1,21 +1,27 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div className=' w-full shadow-md bg-gray-50 '>
-         
-         <nav className='flex text-2xl list-none justify-between p-5'>
-             <div> 
-                 <li><a href='#'>Logo</a></li>
-             </div>
-             <div className='flex space-x-12 ml-12'>
-                 <li><a href='#'>Home</a></li>
-                 <li><a href='#'>About</a></li>
-                 <li><a href='#'>Contact Us</a></li>
-                 <li><a href='#'>Login</a></li>
-             </div>
-         </nav>
+    return (
+        <div>
+            <div className=' w-full shadow-md bg-gray-50 '>
 
-    </div>
-  )
+                <nav className='flex text-2xl list-none justify-between p-5'>
+                    <div>
+                        <li><a href='#'>Logo</a></li>
+                    </div>
+                    <div className='flex space-x-12 ml-12'>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Contact Us</a></li>
+                        <li><a href='#'>Login</a></li>
+                    </div>
+                </nav>
+           
+            </div>
+            <div className='grid md-4 '>
+
+            </div>
+
+        </div>
+    )
 }
